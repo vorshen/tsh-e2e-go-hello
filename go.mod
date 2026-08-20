9f28d6343008df47f4cdf82dfb2d3f1d85d1bb89
niuma <987733967@qq.com>
2026-08-20 17:49:41 +0800

    Add third-party dependency build coverage

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vorshen/tsh-e2e-go-hello
 
 go 1.23
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+import "github.com/google/uuid"
+
 func main() {
+	id := uuid.MustParse("4f51c3d2-d7c7-4ef8-98d0-1ca621f45595")
+	if id.Version() != 4 {
+		panic("fixture UUID is not version 4")
+	}
 	fmt.Println("tsh-e2e-go-ok")
 }
